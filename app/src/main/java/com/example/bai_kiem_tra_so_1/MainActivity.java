cd49749fb5ecbc2b57a5f0959d3e42c52f3000b2
HaoNguyen1903 <nhoxquyam@gmail.com>
2022-05-17 09:30:43 +0700

    123

---
+++ app/src/main/java/com/example/bai_kiem_tra_so_1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bai_kiem_tra_so_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void xulyThoat(View view){
+        finish();
+    }
+}
